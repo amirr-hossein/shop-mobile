359315588c3c274e1dbbb12bbea8a040279261ab
amirrhossein.ir <amirrhossein.ir@gmail.com>
2024-02-12 11:10:28 +0330

    create project for school

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// slider cart
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// banerslider
+var swiper = new Swiper(".banerslider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
